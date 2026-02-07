f9d196888f2195d0dcf54d413e75287dd21fd5ea
Mushfiqur-Rahman-Robin <u1604123@student.cuet.ac.bd>
2026-02-07 09:54:38 +0600

    chore: updated the cross origins

--- src/multi_agent_rag/core/config.py
+++ src/multi_agent_rag/core/config.py
@@ -18,7 +18,10 @@ ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://0.0.0.0:8000",
+    "http://mushfiqur.xyz",
     "https://mushfiqur.xyz",
+    "http://chat.mushfiqur.xyz",
+    "https://chat.mushfiqur.xyz",
 ]
 
 # Models
